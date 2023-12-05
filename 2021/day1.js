8a25fdbd480bbc7ff10115c791759cde04872994
Tim Spickermann <tim@timsp.de>
2023-12-05 13:20:53 +0100

    removed my solutions

--- 2021/day1.js
+++
@@ -1,4 +0,0 @@
-const data = require('fs').readFileSync('data/' + require('path').basename(__filename, '.js'), 'utf-8').split('\n').map(Number);
-
-console.log(data.map((el,i,arr) => el > arr[i - 1]).reduce((a, b) => a + b, 0));
-console.log(data.map((el, i, arr) => el + arr[i + 1] + arr[i + 2]).map((el,i,arr) => el > arr[i - 1]).reduce((a, b) => a + b, 0));
